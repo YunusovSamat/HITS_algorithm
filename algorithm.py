fa8108bcce0d57e3b7618ebb28b36edc19e3e143
YunusovSamat <samat.yunusov@bk.ru>
2019-12-02 20:48:25 +0300

    Chenged algorithm.py. Data output through a loop

--- algorithm.py
+++ algorithm.py
@@ -1,31 +1,28 @@
+import math
+from functools import reduce
+
 import numpy
 
+
+def check_auth(v):
+    sum_sqr = reduce(lambda x, y: x + y**2, v, 0)
+    return reduce(lambda x, y: x + (y/math.sqrt(sum_sqr))**2, v, 0)
+
+
 page_n = 4
-auth = [[1] * page_n]
+auth = list()
 hub = [[1] * page_n]
 
 A = numpy.array([
-    [0, 1, 1, 0],
-    [0, 0, 0, 1],
-    [0, 0, 0, 1],
-    [0, 0, 0, 0]
+    [0, 1, 1, 1],
+    [0, 0, 1, 1],
+    [1, 0, 0, 1],
+    [0, 0, 0, 1]
 ], dtype='byte')
 At = A.transpose()
 
-for i in range(10):
+for i in range(3):
     auth.append(At.dot(hub[-1]))
     hub.append(A.dot(auth[-1]))
-    print('{:>6}|{:>6}|{:>6}'.format(*auth[-1]))
-
-# for i in range(9):
-#     if not any(auth[i] % auth[i+1]):
-#         print('{:>6}|{:>6}|{:>6}'.format(*(auth[i] / auth[i+1])))
-
-
-# while True:
-#     auth.append(At.dot(hub[-1]))
-#     hub.append(A.dot(auth[-1]))
-#
-#     if not any(auth[-1] % auth[-2]):
-#         print(auth[-2])
-#         break
+    print('{:>6}|{:>6}|{:>6}{:>6}'.format(*auth[-1]))
+    print('{:>6}|{:>6}|{:>6}{:>6}'.format(*hub[-1]))
